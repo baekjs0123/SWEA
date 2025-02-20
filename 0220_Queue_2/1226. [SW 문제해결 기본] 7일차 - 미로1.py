0e9fd079627645275568cc0c8d605557327b6e6c
DESKTOP-43O7LPO\baekj <baekjs0123@naver.com>
2025-02-21 00:33:07 +0900

    0220 과제 풀이 및 래더1 풀이

--- 0220_Queue_2/1226. [SW 문제해결 기본] 7일차 - 미로1.py
+++ 0220_Queue_2/1226. [SW 문제해결 기본] 7일차 - 미로1.py
@@ -1,4 +1,45 @@
+from collections import deque
+
+# 미로 범위 유효성 검사
+def is_valid(i, j):
+    return 0 <= i < 16 and 0 <= j < 16
+
+# bfs
+def bfs():
+    # 방문한 구역 체크하기 위한 visited
+    visited = [[0] * 16 for _ in range(16)]
+    # 덱 생성
+    q = deque()
+    # 덱에 처음 출발 위치인 [1, 1] enqueue
+    q.append([1, 1])
+    # 덱에 담자마자 출발지 1로 방문했다고 체크
+    visited[1][1] = 1
+
+    while q:
+        # 현재 위치 pop
+        now_i, now_j = q.popleft()
+        # 현재 위치가 '3'인 경우 골인
+        if maze[now_i][now_j] == '3':
+            # 도달 가능 여부만 출력하면 되므로 1리턴
+            return 1
+        # 델타 탐색하며 다음 위치 확인
+        for di, dj in [[0,1],[1,0],[0, -1],[-1,0]]:
+            next_i, next_j = now_i  + di, now_j + dj
+            # 1. 다음 위치가 미로 범위 안이고
+            # 2. 벽이 아니며
+            # 3. 방문 한 적이 없을 때
+            # enqueue
+            if is_valid(next_i, next_j) and maze[next_i][next_j] != '1' and visited[next_i][next_j] == 0:
+                # 위 조건을 만족할 때마다 enqueue
+                q.append([next_i, next_j])
+                # enqueue와 동시에 방문기록
+                visited[next_i][next_j] += 1
+    # while문이 끝날 때 까지 return을 하지 않았다는 것은
+    # q가 공백일 때 까지 길을 못찾았다는 뜻 = return 0
+    return  0
+
 for _ in range(10):
     tc = int(input())
     maze = [list(map(str, input())) for _ in range(16)]
-    # print(maze)
+    ans = bfs()
+    print(f'#{tc} {ans}')
---
+++ 2월 알고 스터디/[SW 문제해결 기본] 2일차 - Ladder1.py
@@ -0,0 +1,49 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+'''
+접근법
+먼저 골인지점 2를 찾는다. 골인 지점으로부터 사다리의 맨 위까지 거꾸로 거슬러 올라간다.
+쭉 올라가다가 좌, 우에 1이 있으면 1이 있는 방향으로 꺾어서 직진한다.
+쭉 직진하다가 0을 만나면 위로 다시 직진한다.
+반복하면서 i(행번호)가 0이 되면 출발점 찾았다!! j(열번호)를 출력해준다.
+'''
+# 골인지점 찾는 함수
+def find_goal():
+    for j in range(100):
+        if ladder[99][j] == 2:
+            return j
+
+# 유효성 검사
+def is_valid(i, j):
+    return 0 <= i < 100 and 0 <= j < 100 and ladder[i][j] == 1
+
+
+for _ in range(10):
+    tc = int(input())
+    ladder = [list(map(int, input().split())) for _ in range(100)]
+    goal = find_goal()
+
+    # 골인 지점으로부터 거슬러 올라가기 때문에 i = 99, j = goal
+    i, j = 99, goal
+
+    # i가 0이 될때 까지 반복문
+    while i >= 0:
+        # 왼쪽에 길이 있을 때
+        if is_valid(i, j - 1):
+            while is_valid(i, j - 1):
+                j -= 1
+            i -= 1
+        # 오른쪽에 길이 있을 때
+        elif is_valid(i, j + 1):
+            while is_valid(i, j + 1):
+                j += 1
+            i -= 1
+        # 양쪽다 길이 없을 때
+        else:
+            i -= 1
+
+    print(f'#{tc} {j}')
+
+
+
