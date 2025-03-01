ffbdadb89673b8679b0fa15f659b252f8f949d41
Jongseok Baek <baekjs0123@naver.com>
2025-03-01 20:48:09 +0900

    0301 swea 문제 풀이 11039 사각형 찾기 풀이중

--- IM 연습문제/11039. 사각형 찾기.py
+++ IM 연습문제/11039. 사각형 찾기.py
@@ -11,8 +11,33 @@ for tc in range(1, T + 1):
     세로길이 역시 동일한 방법으로 세로의 최대길이를 측정한다.
     가로 * 세로의 넓이를 구하고 이 넓이가 최대값여부인지 확인하고 최대값을 출력한다.      
     '''
+    max_width = 0
+    max_height = 0
+    # extent = 0
+    # max_extent = 0
     for r in range(N):
+        width = 0
+        height = 0
         for c in range(N):
             if arr[r][c] == 1:
-                nr = r + 1
-                nc = c + 1
+                width += 1
+                height += 1
+                for i in range(r, N):
+                    if arr[c][i]:
+                        height += 1
+                    else:
+                        if max_height < height:
+                            max_height = height
+                        height = 0
+                        continue
+                for j in range(c, N):
+                    if arr[r][j]:
+                        width += 1
+                    else:
+                        if max_width < width:
+                            max_width = width
+                        width = 0
+                        continue
+    print(max_width, max_height)
+
+
